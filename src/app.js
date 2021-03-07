ab82e74a32d290fb3968a34db48582ca28137864
devookim <khwoo8026@gmail.com>
2021-03-08 01:42:38 +0900

    user, playlist 모델 생성

--- src/app.js
+++ src/app.js
@@ -1,9 +1,76 @@
 const express = require("express");
+const morgan = require("morgan");
+
+const { sequelize } = require("./models");
+const User = require("./models/user");
+const Playlist = require("./models/playlist");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello");
+app.set("port", process.env.PORT || 3001);
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+sequelize
+  .sync({ force: true })
+  .then(() => {
+    console.log("mysql connect ok");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.get("/", async (req, res) => {
+  try {
+    const user = await User.create({
+      email: "email@email.com",
+      nick: "nick",
+      provider: "provider",
+      oAuthId: "oAuth",
+    });
+    const user2 = await User.create({
+      email: "email2@email.com",
+      nick: "nick2",
+      provider: "provider",
+      oAuthId: "oAuth2",
+    });
+    const playlist = await Playlist.create({
+      title: "title",
+      owner: user.dataValues.nick,
+    });
+    const playlist2 = await Playlist.create({
+      title: "title2",
+      owner: user.dataValues.nick,
+    });
+    const playlist3 = await Playlist.create({
+      title: "title2",
+      owner: user2.dataValues.nick,
+    });
+
+    const model = await User.findOne({
+      include: [
+        {
+          model: Playlist,
+          where: {
+            owner: "nick",
+          },
+        },
+      ],
+    });
+
+    console.log(model.Playlists);
+    // res.status(201).json({ user, user2, playlist, playlist2, playlist3 });
+    res.status(201).json({ model });
+  } catch (err) {
+    console.error(err);
+    res.status(404).send({ err });
+  }
+  // res.send("hello");
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("Bad request");
 });
 
 app.listen(process.env.PORT, () => {
---
+++ src/models/user.js
@@ -0,0 +1,48 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        id: {
+          type: DataTypes.UUID,
+          defaultValue: Sequelize.UUIDV4,
+          allowNull: false,
+          primaryKey: true,
+        },
+        email: {
+          type: DataTypes.STRING(40),
+          allowNull: false,
+          validate: {
+            isEmail: true,
+          },
+          unique: true,
+        },
+        nick: {
+          type: DataTypes.STRING(20),
+          allowNull: false,
+          unique: true,
+          // primaryKey: true,
+        },
+        provider: {
+          type: DataTypes.STRING(10),
+          allowNull: false,
+        },
+        oAuthId: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+
+  static associate(db) {
+    db.User.hasMany(db.Playlist, { foreignKey: "owner", sourceKey: "nick" });
+  }
+};
