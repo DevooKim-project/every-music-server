ca9b4eacdac860aea966213e1c337a3d8fd58e9e
devookim <khwoo8026@gmail.com>
2021-03-17 10:44:15 +0900

    [Fix] rename middleWare

--- src/router/playlist/youtube/controller.js
+++ src/router/playlist/youtube/controller.js
@@ -4,7 +4,7 @@ const { youtubeService } = require("../../../services/playlist");
 const { tokenService } = require("../../../services/database");
 const { parseToken } = require("../../../middleware/auth");
 
-exports.searchList = async (req, res) => {
+exports.searchPlayList = async (req, res) => {
   try {
     const localToken = parseToken(req.headers.authorization);
     const payload = jwt.verify(localToken, process.env.JWT_SECRET);
@@ -26,7 +26,7 @@ exports.searchList = async (req, res) => {
   }
 };
 
-exports.getItems = async (req, res) => {
+exports.getPlayListItem = async (req, res, next) => {
   try {
     const localToken = parseToken(req.headers.authorization);
     const payload = jwt.verify(localToken, process.env.JWT_SECRET);
@@ -36,23 +36,29 @@ exports.getItems = async (req, res) => {
       type: "access",
     });
     let { playLists } = req.body;
-    // console.log(playLists);
-    // playLists = playLists.split(" ");
     const tracks = [];
 
     for (const playList of playLists) {
       const id = playList.id;
-      console.log("id: ", id);
       const item = await youtubeService.getItems(id, accessToken, " ");
-      // console.log("item: ", item);
-      // tracks = item.contentDetails.videoId;
-      console.log("item: ", item.length);
       tracks.push(item);
     }
-    console.log("tracks: ", tracks.length);
-    res.send(tracks);
+
+    req.tracks = tracks;
+    next();
+    // res.send(tracks);
   } catch (error) {
     console.error(error);
     res.send(error);
   }
 };
+
+exports.getTrackInfo = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+    res.send(error);
+  }
+};
+
+exports.structTrack = async (req, res) => {};
--- src/router/playlist/youtube/index.js
+++ src/router/playlist/youtube/index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 
-const { searchList, getItems } = require("./controller");
+const {
+  searchPlayList,
+  getPlayListItem,
+  getTrackInfo,
+  structTrack,
+} = require("./controller");
 
 const router = express.Router();
 
-router.get("/searchList", searchList);
-router.get("/getItems", getItems);
-router.get("/getTrack");
-router.get("/struct");
+router.get("/playLists", searchPlayList);
+router.get("/playListItems", getPlayListItem, getTrackInfo, structTrack);
+// router.get("/tracks");
+// router.get("/struct");
 router.get("/insert");
 
 module.exports = router;
