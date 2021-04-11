7feb11750a6d6852d10f07ac831e90848c9b3ff6
devookim <khwoo8026@gmail.com>
2021-04-12 00:17:20 +0900

    [Refactor] remove not used file

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,7 +1,5 @@
-const sequelize = require("../database/models");
 const mongodb = require("../database/schema");
 
 module.exports = () => {
-  sequelize();
   mongodb();
 };
--- src/services/database/index.js
+++ src/services/database/index.js
@@ -1,14 +1,11 @@
-// const { createUser, findOneUser, destroyUser } = require("./user");
 const userService = require("./user");
 const tokenService = require("./token");
-const cacheService = require("./cache");
 const trackService = require("./track");
 const artistService = require("./artist");
 const playlistService = require("./playlist");
 
 exports.userService = userService;
 exports.tokenService = tokenService;
-exports.cacheService = cacheService;
 exports.trackService = trackService;
 exports.artistService = artistService;
 exports.playlistService = playlistService;
