d101ffce23a6df70da7e0bf4b8e6f909b4590977
devookim <khwoo8026@gmail.com>
2021-04-24 23:03:01 +0900

    [Fix] paginate

--- src/utils/paginate.js
+++ src/utils/paginate.js
@@ -1,18 +1,12 @@
 const paginate = async (schema, filter, options) => {
-  const limit =
-    options.limit && parseInt(options.limit, 10) > 0
-      ? parseInt(options.limit, 10)
-      : 10;
-  const page =
-    options.page && parseInt(options.page, 10) > 0
-      ? parseInt(options.page, 10)
-      : 1;
+  const limit = options.limit && parseInt(options.limit, 10) > 0 ? parseInt(options.limit, 10) : 10;
+  const page = options.page && parseInt(options.page, 10) > 0 ? parseInt(options.page, 10) : 1;
   const skip = (page - 1) * limit;
 
   const countPromise = schema.countDocuments().exec();
   const docsPromise = schema.find(filter).skip(skip).limit(limit);
 
-  return Promise.all([countPromise, docsPromise]).them((values) => {
+  return Promise.all([countPromise, docsPromise]).then((values) => {
     const [totalResults, results] = values;
     const totalPages = Math.ceil(totalResults / limit);
     const result = {
