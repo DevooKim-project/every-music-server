d62d99273daba3aa62b07186938e3ffe1470b70d
devookim <khwoo8026@gmail.com>
2021-04-25 00:01:23 +0900

    [Fix] read library

--- src/router/user/index.js
+++ src/router/user/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { tokenTypes } = require("../../config/type");
 
+const { tokenTypes } = require("../../config/type");
 const verifyToken = require("../../middleware/auth");
 const controller = require("./controller");
 
 const router = express.Router();
 
-router.get("/library/:userId", verifyToken(tokenTypes.ACCESS), controller.getLibrary);
+router.get("/library", verifyToken(tokenTypes.ACCESS), controller.getLibrary);
 
 module.exports = router;
