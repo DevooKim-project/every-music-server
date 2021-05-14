4c2e5e1e492420cc042ee9df1bf3fc244694fc6b
devookim <khwoo8026@gmail.com>
2021-05-15 02:45:10 +0900

    [Add] 401 error handling in convert

--- src/router/convert/controller.js
+++ src/router/convert/controller.js
@@ -1,12 +1,17 @@
 const { switchConvertPlatform } = require("../../utils/switchPlatform");
 const catchAsync = require("../../utils/catchAsync");
 const { tokenService } = require("../../services");
+const ApiError = require("../../utils/ApiError");
+const httpStatus = require("http-status");
 
-const getPlatformTokenByUserId = async (req, res, next) => {
+const getPlatformTokenByUserId = catchAsync(async (req, res, next) => {
   const platformToken = await tokenService.getPlatformTokenByUserId(req.payload.id, req.params.platform);
+  if (!platformToken.accessToken) {
+    throw new ApiError(httpStatus.UNAUTHORIZED);
+  }
   req.platformToken = platformToken;
   return next();
-};
+});
 const getPlaylistFromPlatform = catchAsync(async (req, res) => {
   const controller = switchConvertPlatform(req.params.platform);
   return controller.getPlaylistFromPlatform(req, res);
