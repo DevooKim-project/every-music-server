07a517b9669fdec393c3646ce482f71d10133a49
devookim <khwoo8026@gmail.com>
2021-06-18 22:36:26 +0900

    [Refactor] google oauth scope change

--- src/config/authorizationUrl.js
+++ src/config/authorizationUrl.js
@@ -5,9 +5,9 @@ const googleAuthorizationUrl = (redirectUri) => {
   const scopes = [
     "https://www.googleapis.com/auth/userinfo.email",
     "https://www.googleapis.com/auth/userinfo.profile",
-    "https://www.googleapis.com/auth/youtube.readonly",
-    "https://www.googleapis.com/auth/youtube.upload",
-    "https://www.googleapis.com/auth/youtube.force-ssl",
+    // "https://www.googleapis.com/auth/youtube.readonly",
+    // "https://www.googleapis.com/auth/youtube.upload",
+    // "https://www.googleapis.com/auth/youtube.force-ssl",
     "https://www.googleapis.com/auth/youtube",
   ];
 
